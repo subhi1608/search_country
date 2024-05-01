381139cb47481b6feda142626c1335ddee480c70
subhi1608 <sarveshb.bhatt@gmail.com>
2024-05-01 08:38:14 +0530

    first commit

---
+++ src/components/inputField.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputField = () => {
+	return (
+		<div>
+			<input type="text" name="" id="" placeholder="Enter a city" />
+		</div>
+	);
+};
+
+export default InputField;
---
+++ src/components/tableWrapper.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TableWrapper = () => {
+	return (
+		<div>
+			<table>
+				<thead>APP</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>A</td>
+						<td>C</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>A</td>
+						<td>C</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>A</td>
+						<td>C</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>A</td>
+						<td>C</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default TableWrapper;
